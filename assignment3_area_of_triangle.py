504f6e03865fa4234722af73b3dd5aab7e764163
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-18 21:21:31 +0530

    Add files via upload

---
+++ assignment3_area_of_triangle.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "area of triangle 12.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "b=4\n",
+    "h=6\n",
+    "area=(b*h)/2\n",
+    "print(\"area of triangle\",area)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
