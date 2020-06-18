8d1b726d6efdd3df22edf95d7880690638282a17
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-18 22:02:07 +0530

    Add files via upload

---
+++ assignment8_circumference_of_circle.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "circumference of circle is  25.132741228718345\n"
+     ]
+    }
+   ],
+   "source": [
+    "import math\n",
+    "radius=4\n",
+    "circumference=2*math.pi*radius\n",
+    "print(\"circumference of circle is \",circumference)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
