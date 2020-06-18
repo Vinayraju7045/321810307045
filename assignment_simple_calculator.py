c8a31adad11ce870d18d255157d8b1bccbfe09f4
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-18 20:44:00 +0530

    Add files via upload

---
+++ assignment_simple_calculator.py
@@ -0,0 +1,84 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "6\n",
+      "2\n",
+      "8\n",
+      "2.0\n",
+      "0\n",
+      "16\n",
+      "2\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=4\n",
+    "b=2\n",
+    "sum=a+b\n",
+    "print(+sum)\n",
+    "subtract=a-b\n",
+    "print(+subtract)\n",
+    "multiply=a*b\n",
+    "print(+multiply)\n",
+    "divide=a/b\n",
+    "print(+divide)\n",
+    "modulus=a%b\n",
+    "print(+modulus)\n",
+    "exponent=a**b\n",
+    "print(+exponent)\n",
+    "floordivide=a//b\n",
+    "print(+floordivide)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
